eabf2bb896962dee4814fddeac1b28fe55adc801
Cao Tengfei <tengfei.cao@anlogic.com>
2022-11-14 16:32:04 +0800

    [ES-180][feat][application][apu_handoff] support aarch32 handoff [rootcause]:support aarch32 handoff [side effect]:none
    Change-Id: If740562f21d22c39f6754a26f0df1a2bb7de30c1

--- application/fsbl/alfsbl_handoff.c
+++ application/fsbl/alfsbl_handoff.c
@@ -10,7 +10,9 @@
 #include "alfsbl_hw.h"
 #include "alfsbl_handoff.h"
 
-void AlFsbl_HandoffExit(uint64_t HandoffAddress)
+#define AARCH_64
+
+void __attribute__((noinline)) AlFsbl_HandoffExit(uint64_t HandoffAddress)
 {
 	//printf("Exit from FSBL\r\n");
 #if __riscv
@@ -20,18 +22,30 @@ void AlFsbl_HandoffExit(uint64_t HandoffAddress)
 	:[src]"r"(HandoffAddress)
 	);
 #else
-	__asm__ __volatile__("mov x30, %0"::"Jr"(HandoffAddress):);/* move the destination address into x30 register */
-	__asm__ __volatile__("tlbi ALLE3":::);/* invalidate All E3 translation tables */
-	__asm__ __volatile__("ic IALLU":::);/* invalidate I Cache All to PoU, Inner Shareable */
-	__asm__ __volatile__("dsb sy":::);
-	__asm__ __volatile__("isb" : : : "memory");/* make sure it completes */
-	__asm__ __volatile__("mrs x5, SCTLR_EL3"::: "memory");/* Read control register */
-	__asm__ __volatile__("mov x6, #0x1005":::);/* D, I , M bits disable */
-	__asm__ __volatile__("bic x5, x5, x6":::);/* Disable MMU, L1 and L2 I/D cache */
-	__asm__ __volatile__("msr SCTLR_EL3, x5"::: "memory");
-	__asm__ __volatile__("isb" : : : "memory");
+	__asm__ __volatile__("mov x30, %0"::"r"(HandoffAddress):"x30");/* move the destination address into x30 register */
+	// __asm__ __volatile__("tlbi ALLE3":::);/* invalidate All E3 translation tables */
+	// __asm__ __volatile__("ic IALLU":::);/* invalidate I Cache All to PoU, Inner Shareable */
+	// __asm__ __volatile__("dsb sy":::);
+	// __asm__ __volatile__("isb" : : : "memory");/* make sure it completes */
+	// __asm__ __volatile__("mrs x5, SCTLR_EL3"::: "memory", "x5");/* Read control register */
+	// __asm__ __volatile__("mov x6, #0x1005":::"x6");/* D, I , M bits disable */
+	// __asm__ __volatile__("bic x5, x5, x6":::"x5", "x6");/* Disable MMU, L1 and L2 I/D cache */
+	// __asm__ __volatile__("msr SCTLR_EL3, x5"::: "memory", "x5");
+	// __asm__ __volatile__("isb" : : : "memory");
 	//__asm__ __volatile__("msr CurrentEL, #0xc":::);
-	__asm__ __volatile__("br x30":::);
+#ifndef AARCH_64
+	__asm__ __volatile__("MSR      SCTLR_EL2, XZR":::"memory");
+	__asm__ __volatile__("MSR      SCTLR_EL1, XZR":::"memory");
+	__asm__ __volatile__("MSR      SCR_EL3, XZR":::"memory");	/* AArch32 */
+	//__asm__ __volatile__("LDR      x1, x30":::"x1","x30");
+	__asm__ __volatile__("MSR      ELR_EL3, x30":::"x30", "memory");
+	__asm__ __volatile__("MOV      x1, 0x13":::"x1");
+	__asm__ __volatile__("msr spsr_el3, x1":::"x1", "memory");
+	__asm__ __volatile__("eret":::);
+
+#else
+	__asm__ __volatile__("br x30":::"x30");
+#endif
 #endif
 }
 
@@ -58,11 +72,8 @@ uint32_t AlFsbl_Handoff(const AlFsblInfo *FsblInstancePtr)
 	for(HandoffIdx = 0; HandoffIdx < FsblInstancePtr->HandoffCpuNum; HandoffIdx++) {
 		CpuSettings = FsblInstancePtr->HandoffValues[HandoffIdx].CpuSettings;
 		HandoffAddress = FsblInstancePtr->HandoffValues[HandoffIdx].HandoffAddress;
-		//printf("handoff cpu : %08x\r\n", FsblInstancePtr->HandoffValues[HandoffIdx].CpuSettings);
-		//printf("handoff addr: %08x\r\n", FsblInstancePtr->HandoffValues[HandoffIdx].HandoffAddress);
-
-		printf("handoff cpu : %08x\r\n", CpuSettings);
-		printf("handoff addr: %08x\r\n", HandoffAddress);
+		printf("handoff cpu : %08x\n", FsblInstancePtr->HandoffValues[HandoffIdx].CpuSettings);
+		printf("handoff addr: %08x\n", FsblInstancePtr->HandoffValues[HandoffIdx].HandoffAddress);
 
 		if(RunningCpu != CpuSettings) {
 			/// handoff to a different cpu
