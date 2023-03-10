6db5202abd00f1dfd3d5327485e4ecd47748f6c3
ybhe <yongbo.he@anlogic.com>
2023-03-10 15:15:48 +0800

    [fix][driver][uart] print out autotest pass if work properly;
    Change-Id: I4e51d4a2ed67a9ed3db14caefff28125eec9ca1d

--- application/baremetal/AL_demo_dmac/AL_dmac_PER/AL_dmac_uart/main.c
+++ application/baremetal/AL_demo_dmac/AL_dmac_PER/AL_dmac_uart/main.c
@@ -18,34 +18,37 @@
  *
 */
 volatile uint16_t count = 0;
+
 void SOC_DMA_AHB_HANDLER(void)
 {
-    //printf("enter_irq_number%d\n\r",count);        
-      count ++;
-     if(count == 10){
-       AlDma_SetSingle(AL_dmac_channel_num_1,Dmac_src_dst);
-       AlDma_ClearIrq(AL_DMAC);
-       AlDma_Enable(AL_DMAC);
-       //count = 0;
-       //printf("complete_DMAC_transfer\n\r");    
-    }
-    else{
-           AlDma_ClearIrq(AL_DMAC);
-         }  
+    //printf("enter_irq_number%d\n\r",count);
+	count ++;
+	if(count == 10) {
+		AlDma_SetSingle(AL_dmac_channel_num_1,Dmac_src_dst);
+		AlDma_ClearIrq(AL_DMAC);
+		AlDma_Enable(AL_DMAC);
+
+		printf("complete_DMAC_transfer\n\r");
+	}
+	else {
+		AlDma_ClearIrq(AL_DMAC);
+	}
 }
-int main(void){
-     volatile uint32_t buffer[256];
-	 for (volatile uint32_t i = 0 ; i < 256; i++)
-	 {
-		 buffer[i] = i;
-	 }
-	 write_To_OCM((uint32_t*)buffer,256,(uint32_t*)MEM_BASE1_ADDR);
-     Enablepinmux1();
-	 AlUart_SetTxTrigger(AL_UART0,FIFO_1_2full);
-	 AlUart_SetDmaMode(AL_UART0);
-	 AlUart_DmaInit(AL_DMAC_channel_1,uart0,tx,Dmac_transfer_row1,50);
-	 AlDma_Enable(AL_DMAC);
-	 AlDma_EnableChannel(AL_DMAC,AL_dmac_channel_num_1);
+
+int main(void)
+{
+	uint32_t buffer[256];
+
+	strcpy(buffer, "[AUTOTEST]:[DMAC]:[UART]:[PASS]\r\n");
+
+	write_To_OCM((uint32_t*)buffer, 256, (uint32_t*)MEM_BASE1_ADDR);
+
+	AlUart_SetTxTrigger(AL_UART0, FIFO_1_2full);
+	AlUart_SetDmaMode(AL_UART0);
+	AlUart_DmaInit(AL_DMAC_channel_1, uart0, tx, Dmac_transfer_row1, 50);
+	AlDma_Enable(AL_DMAC);
+	AlDma_EnableChannel(AL_DMAC, AL_dmac_channel_num_1);
+
 	return 0;
 }
 
