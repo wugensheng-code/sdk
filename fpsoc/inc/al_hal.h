b0654721b5a90768e6866cdec0c6e80785223c63
xiaoyong wang <xiaoyong.wang@anlogic.com>
2023-07-17 08:20:54 +0000

    [ES-231][feat][MPU/GPIO/SMC] add driver headfiles in al_hal.h; define if driver support in al_params.h

--- fpsoc/inc/al_hal.h
+++ fpsoc/inc/al_hal.h
@@ -7,20 +7,44 @@ extern "C" {
 
 #include "al_core.h"
 
-#ifdef HAVE_UARTPS_DRIVER
-#include "al_uart_hal.h"
+#ifdef HAVE_CANPS_DRIVER
+#include "al_can_hal.h"
+#endif
+
+#ifdef HAVE_DMACAHBPS_DRIVER
+#include "al_dmacahb_hal.h"
+#endif
+
+#ifdef HAVE_GPIOPS_DRIVER
+#include "al_gpio_hal.h"
+#endif
+
+#ifdef HAVE_IICPS_DRIVER
+#include "al_iic_hal.h"
+#endif
+
+#ifdef HAVE_MMCPS_DRIVER
+#include "al_mmc_hal.h"
+#endif
+
+#ifdef HAVE_MPUPS_DRIVER
+#include "al_mpu_hal.h"
+#endif
+
+#ifdef HAVE_QSPIPS_DRIVER
+#include "al_qspi_hal.h"
+#endif
+
+#ifdef HAVE_SMCPS_DRIVER
+#include "al_smc_hal.h"
 #endif
 
 #ifdef HAVE_SPIPS_DRIVER
 #include "al_spi_hal.h"
 #endif
 
-#ifdef HAVE_CANPS_DRIVER
-#include "al_can_hal.h"
-#endif
-
-#ifdef HAVE_IICPS_DRIVER
-#include "al_iic_hal.h"
+#ifdef HAVE_UARTPS_DRIVER
+#include "al_uart_hal.h"
 #endif
 
 #ifdef __cplusplus
