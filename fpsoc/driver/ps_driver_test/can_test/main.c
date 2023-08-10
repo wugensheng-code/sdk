d851387a6314ca3fbe1654bf6442ff1383390467
Cao Tengfei <tengfei.cao@anlogic.com>
2023-08-10 15:58:53 +0800

    [ES-285][fix][driver][CAN] fix test case recv flow
    Change-Id: I867337833bb6dbe42230b65cec65ee1da7715ec1

--- fpsoc/driver/ps_driver_test/can_test/main.c
+++ fpsoc/driver/ps_driver_test/can_test/main.c
@@ -319,6 +319,7 @@ static AL_VOID AlCan_Test_ExLoopBack(AL_VOID)
 {
     AL_CAN_HalStruct Handle;
     AL_CAN_InitStruct Config;
+    AL_CAN_FrameStruct Frame;
     AL_U32 DeviceId = 1;
     AL_U32 Ret = AL_OK;
     AL_U32 Timeout = 10000000;
@@ -342,72 +343,132 @@ static AL_VOID AlCan_Test_ExLoopBack(AL_VOID)
         if (Ret != AL_OK) {
             AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame1 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame1 example\r\n");
         AlSys_MDelay(500);
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame3, Timeout);
         if (Ret != AL_OK) {
-            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame3 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame3 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame3 example\r\n");
         AlSys_MDelay(500);
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame5, Timeout);
         if (Ret != AL_OK) {
-            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame5 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame5 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame5 example\r\n");
         AlSys_MDelay(500);
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame7, Timeout);
         if (Ret != AL_OK) {
-            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame7 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame7 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame7 example\r\n");
         AlSys_MDelay(500);
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame9, Timeout);
         if (Ret != AL_OK) {
-            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame9 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame9 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame9 example\r\n");
         AlSys_MDelay(500);
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame11, Timeout);
         if (Ret != AL_OK) {
-            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame11 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame11 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame11 example\r\n");
         AlSys_MDelay(500);
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame13, Timeout);
         if (Ret != AL_OK) {
-            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame13 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame13 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame13 example\r\n");
         AlSys_MDelay(500);
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame15, Timeout);
         if (Ret != AL_OK) {
-            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame15 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame15 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame15 example\r\n");
         AlSys_MDelay(500);
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame16, Timeout);
         if (Ret != AL_OK) {
-            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame16 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame16 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame16 example\r\n");
         AlSys_MDelay(500);
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame17, Timeout);
         if (Ret != AL_OK) {
-            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame17 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame17 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame17 example\r\n");
         AlSys_MDelay(500);
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame18, Timeout);
         if (Ret != AL_OK) {
-            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame18 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame18 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame18 example\r\n");
         AlSys_MDelay(500);
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame19, Timeout);
         if (Ret != AL_OK) {
-            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame19 Error:0x%x\r\n", Ret);
         }
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame19 Error:0x%x\r\n", Ret);
+        }
+        AlCan_Dev_DisplayFrame(&Frame);
         AL_LOG(AL_LOG_LEVEL_DEBUG, "Can 2.0B Frame19 example\r\n");
         AlSys_MDelay(500);
     }
@@ -445,6 +506,7 @@ static AL_VOID AlCan_Test_StdIntr(AL_VOID)
 {
     AL_CAN_HalStruct Handle;
     AL_CAN_InitStruct Config;
+    AL_CAN_FrameStruct Frame;
     AL_CAN_CallBackStruct CallBack;
     AL_U32 DeviceId = 1;
     AL_U32 Ret = AL_OK;
@@ -462,13 +524,19 @@ static AL_VOID AlCan_Test_StdIntr(AL_VOID)
     CallBack.Func = AlCan_Test_EventCallBack;
     CallBack.Ref = &Handle;
 
-    Ret = AlCan_Hal_Init(&Handle, &Config, &CallBack, DeviceId);
+    Ret = AlCan_Hal_Init(&Handle, &Config, AL_NULL, DeviceId);
     if (Ret != AL_OK) {
         AL_LOG(AL_LOG_LEVEL_ERROR, "Hal Init Error:0x%x\r\n", Ret);
     }
     AlIntr_SetGlobalInterrupt(AL_FUNC_ENABLE);
 
     while (1) {
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame Error:0x%x\r\n", Ret);
+        } else {
+            AlCan_Dev_DisplayFrame(&Frame);
+        }
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &Frame1, Timeout);
         if (Ret != AL_OK) {
             AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
@@ -547,6 +615,7 @@ static AL_VOID AlCan_Test_FdIntr(AL_VOID)
 {
     AL_CAN_HalStruct Handle;
     AL_CAN_CallBackStruct CallBack;
+    AL_CAN_FrameStruct Frame;
     AL_U32 DeviceId = 1;
     AL_U32 Ret = AL_OK;
     AL_U32 Timeout = 10000000;
@@ -556,13 +625,19 @@ static AL_VOID AlCan_Test_FdIntr(AL_VOID)
     CallBack.Func = AlCan_Test_EventCallBack;
     CallBack.Ref = &Handle;
 
-    Ret = AlCan_Hal_Init(&Handle, AL_NULL, &CallBack, DeviceId);
+    Ret = AlCan_Hal_Init(&Handle, AL_NULL, AL_NULL, DeviceId);
     if (Ret != AL_OK) {
         AL_LOG(AL_LOG_LEVEL_ERROR, "Hal Init Error:0x%x\r\n", Ret);
     }
     AlIntr_SetGlobalInterrupt(AL_FUNC_ENABLE);
 
     while (1) {
+        Ret = AlCan_Hal_RecvFrame(&Handle, &Frame);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame Error:0x%x\r\n", Ret);
+        } else {
+            AlCan_Dev_DisplayFrame(&Frame);
+        }
         Ret = AlCan_Hal_SendFrameBlock(&Handle, &FdFrame1, Timeout);
         if (Ret != AL_OK) {
             AL_LOG(AL_LOG_LEVEL_ERROR, "Send Frame1 Error:0x%x\r\n", Ret);
@@ -810,5 +885,12 @@ static AL_VOID AlCan_Test_FdDmaRecv(AL_VOID)
     // AlDmacAhb_Dev_Start(DmacHandle.Channel);
     AlDmacAhb_Hal_Start(&DmacHandle);
 
-    while (1);
+    while (1) {
+        Ret = AlCan_Hal_RecvFrameBlock(&Handle, &Frame, Timeout);
+        if (Ret != AL_OK) {
+            AL_LOG(AL_LOG_LEVEL_ERROR, "Recv Frame Error:0x%x\r\n", Ret);
+        } else {
+            AlCan_Dev_DisplayFrame(&Frame);
+        }
+    }
 }
