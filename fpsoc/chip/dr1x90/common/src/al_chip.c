71741d803b23b17145480cc1dad266861508dea8
ybhe <yongbo.he@anlogic.com>
2023-08-30 20:40:47 +0800

    [ES-109][fix][devops]remove some useless cmake files;

--- fpsoc/chip/dr1x90/common/src/al_chip.c
+++ fpsoc/chip/dr1x90/common/src/al_chip.c
@@ -8,15 +8,6 @@
 
 AL_U64 SystemCoreClock = SYSTEM_CLOCK;  /* System Clock Frequency (Core Clock) */
 
-
-/**
- * \brief early init function before main
- * \details
- * This function is executed right before main function.
- * For RISC-V gnu toolchain, _init function might not be called
- * by __libc_init_array function, so we defined a new function
- * to do initialization
- */
 void _premain_init(void)
 {
     AlChip_Init();
