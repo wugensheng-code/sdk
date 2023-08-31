0129b7ee6118555074260e21ac8e06488288cff8
Cao Tengfei <tengfei.cao@anlogic.com>
2023-08-31 10:44:56 +0800

    [ES-231][fix][infrastructure] board_cfg.mk add AL9000_CLK_CONFIG for call clk_simple_config func
    Change-Id: I37e3d1a7bd568bccfb21e640adbb0f9b4519c674

--- solutions/boards/board_dr1x90_ad101_v10/board_cfg.mk
+++ solutions/boards/board_dr1x90_ad101_v10/board_cfg.mk
@@ -1,5 +1,9 @@
-# AL9000_OSC_33: for chip pll enable and call clk_simple_config func
+# AL9000_CLK_CONFIG: for chip pll enable(same as board cfg pin) and call clk_simple_config func(2:2:2:1)
+# AL9000_OSC_33: for chip select OSC frequency
 # AL9000_BRINGUP: for chip bypass mpu disable flow
 # BOARD_DR1X90_AD101_V10: for demo board select uart 1 as log output
 
-CFLAGS += -DAL9000_BRINGUP -DBOARD_DR1X90_AD101_V10 -DAL9000_OSC_33
+CFLAGS += -DAL9000_CLK_CONFIG
+CFLAGS += -DAL9000_OSC_33
+CFLAGS += -DAL9000_BRINGUP
+CFLAGS += -DBOARD_DR1X90_AD101_V10
