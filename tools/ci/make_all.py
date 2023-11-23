b1f15aa4971c8d3807a3eb953508ee02972f3113
wxy <xiaoyong.wang@anlogic.com>
2023-11-23 09:52:11 +0800

    [ES-507][fix][neon]fix neon compile error

--- tools/ci/make_all.py
+++ tools/ci/make_all.py
@@ -57,6 +57,9 @@ def make_all(path, chip, download, sdk_root, debug):
             if 'freertos' in str(makefile_p) and chip == 'dr1v90':
                 continue
 
+            if 'neon' in str(makefile_p) and chip == 'dr1v90':
+                continue
+
             if 'msc_rtthread' in str(makefile_p) and chip == 'dr1m90':
                 continue
 
