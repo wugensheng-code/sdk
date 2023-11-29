b3e4124a31bd2bdbfb72a60f113abe39991e631f
wxy <xiaoyong.wang@anlogic.com>
2023-11-29 11:45:45 +0800

    [ES-267][fix][json]fix config.mk json and add LOG_DEV=NONE

--- fpsoc/lib/log/src/al_log.c
+++ fpsoc/lib/log/src/al_log.c
@@ -10,20 +10,16 @@
 
 #if ((LOG_DEV == AL_LOG_UART0) || (LOG_DEV == AL_LOG_UART1))
 AL_UART_HalStruct *AlLog;
-#elif (LOG_DEV == NONE)
-
 #endif
 
 AL_S32 AlLog_Init()
 {
 #ifdef LOG_DEV
 
-#if((LOG_DEV == NONE))
+#if((LOG_DEV == AL_LOG_NONE))
     return AL_OK;
-#endif
 
-
-#elif ((LOG_DEV == AL_LOG_UART0) || (LOG_DEV == AL_LOG_UART1))
+#else ((LOG_DEV == AL_LOG_UART0) || (LOG_DEV == AL_LOG_UART1))
     AL_UART_InitStruct UART_InitStruct = {
         .BaudRate           = 115200,
         .Parity             = AL_UART_NO_PARITY,
@@ -33,20 +29,20 @@ AL_S32 AlLog_Init()
         .CharTimeoutEnable  = AL_TRUE
     };
 
-#if (LOG_DEV == AL_LOG_UART0)
-    #ifdef UART0_BAUDRATE
-        UART_InitStruct.BaudRate = UART0_BAUDRATE;
+    #if (LOG_DEV == AL_LOG_UART0)
+        #ifdef UART0_BAUDRATE
+            UART_InitStruct.BaudRate = UART0_BAUDRATE;
+        #endif
     #endif
-#endif
-#if (LOG_DEV == AL_LOG_UART1)
-    #ifdef UART1_BAUDRATE
-        UART_InitStruct.BaudRate = UART1_BAUDRATE;
+    #if (LOG_DEV == AL_LOG_UART1)
+        #ifdef UART1_BAUDRATE
+            UART_InitStruct.BaudRate = UART1_BAUDRATE;
+        #endif
     #endif
-#endif
 
     return AlUart_Hal_Init(&AlLog, LOG_DEV, &UART_InitStruct, AL_NULL);
-#elif((LOG_DEV == NONE))
-    return AL_OK;
+    
+#endif
 #endif
     return AL_OK;
 }
