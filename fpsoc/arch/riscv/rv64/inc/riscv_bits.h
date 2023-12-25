3d8df057d0805d4cbfa6d2b5ffcba6baf332863f
huang jian <jian.huang@anlogic.com>
2023-12-25 09:14:12 +0800

    [ES-545][fix][risc-v-boot]fix riscv fp error

--- fpsoc/arch/riscv/rv64/inc/riscv_bits.h
+++ fpsoc/arch/riscv/rv64/inc/riscv_bits.h
@@ -4,19 +4,31 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-
+#if __riscv_xlen == 64
 # define SLL32                  sllw
 # define STORE                  sd
 # define LOAD                   ld
 # define LWU                    lwu
 # define LOG_REGBYTES           3
+#else
+# define SLL32                  sll
+# define STORE                  sw
+# define LOAD                   lw
+# define LWU                    lw
+# define LOG_REGBYTES           2
+#endif /* __riscv_xlen */
 
-#define REGBYTES                (1 << LOG_REGBYTES)
+#define REGBYTES (1 << LOG_REGBYTES)
 
-#define __STR(s)                #s
-#define STRINGIFY(s)            __STR(s)
-
-#define FPSTORE                fsw
-#define FPLOAD                 flw
-#define LOG_FPREGBYTES         2
+#if defined(__riscv_flen)
+#if __riscv_flen == 64
+# define FPSTORE                fsd
+# define FPLOAD                 fld
+# define LOG_FPREGBYTES         3
+#else
+# define FPSTORE                fsw
+# define FPLOAD                 flw
+# define LOG_FPREGBYTES         2
+#endif /* __riscv_flen == 64 */
 #define FPREGBYTES              (1 << LOG_FPREGBYTES)
+#endif /* __riscv_flen */
