918f3cff3686cbec715d0f61b94106fef5c80967
wangixaoyong <xiaoyong.wang@anlogic.com>
2024-09-14 14:48:55 +0800

    [ES-788][feat][empty]supply an empty project for customer

---
+++ solutions/demo/baremetal/empty/main.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023, Anlogic Inc. and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include "al_core.h"
+#include <stdlib.h>
+
+AL_S32 main()
+{
+    al_printf("SoC Started ... \r\n");
+
+    return 0;
+}
